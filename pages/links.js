8af3aa41ccacd686db6d0b110d6abdb1aa7c38c6
Praveen Nithiyananda <n.praveen28j@gmail.com>
2022-02-16 19:39:52 +0530

    heading update

--- pages/links.js
+++ pages/links.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Links from "../components/Links";
+import Head from "next/head";
 
 export default function links() {
   return (
     <>
+      <Head>
+        <title>Praveen N. - Links</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <main className="max-w-4xl mx-auto mt-16 antialiased">
         <Links></Links>
       </main>
--- pages/moreInfo.js
+++ pages/moreInfo.js
@@ -1,9 +1,14 @@
 import React from "react";
 import MoreInfo from "../components/MoreInfo";
+import Head from "next/head";
 
 export default function links() {
   return (
     <>
+      <Head>
+        <title>Praveen N. - More Information</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <main className="max-w-4xl mx-auto mt-16 antialiased">
         <MoreInfo></MoreInfo>
       </main>
